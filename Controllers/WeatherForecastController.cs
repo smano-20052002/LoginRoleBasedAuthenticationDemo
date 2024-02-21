8cc49bddfa829562179bf27a0fce706ecc5949b4
smano-20052002 <smano4570@gmail.com>
2024-02-21 17:10:40 +0530

    ked

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,3 +1,6 @@
+using LoginRoleBasedAuthenticationDemo.ViewModel;
+using LoginRoleBasedAuthenticationDemo.ViewModel.OtherObject;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LoginRoleBasedAuthenticationDemo.Controllers
@@ -10,7 +13,18 @@ namespace LoginRoleBasedAuthenticationDemo.Controllers
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
+        private static readonly string[] SummariesAdmin = new[]
+       {
+           "Admin"
+        };
+        private static readonly string[] SummariesOwner = new[]
+       {
+            "Owner"
+        };
+        private static readonly string[] SummariesUser = new[]
+       {
+            "User"
+        };
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -24,5 +38,27 @@ namespace LoginRoleBasedAuthenticationDemo.Controllers
         {
             return Ok(Summaries);
         }
+        [HttpGet]
+        [Route("GetUserRole")]
+        [Authorize(Roles = StaticUserRoles.USER)]
+        public IActionResult GetUserRole()
+        {
+            return Ok(SummariesUser);
+        }
+        [HttpGet]
+        [Route("GetAdminRole")]
+        [Authorize(Roles = StaticUserRoles.ADMIN)]
+        public IActionResult GetAdminRole()
+        {
+            return Ok(SummariesAdmin);
+        }
+        [HttpGet]
+        [Route("GetOwnerRole")]
+        [Authorize(Roles = StaticUserRoles.OWNER)]
+        public IActionResult GetOwnerRole()
+        {
+            return Ok(SummariesOwner);
+        }
+        
     }
 }
